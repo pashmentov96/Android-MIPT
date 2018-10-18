e1bc10d8d22a3354e684391936430d25395f2778
nikita <nik.pashmentov@ya.ru>
2018-10-18 09:42:03 +0300

    Homework 5. Generate persons with AsyncTask

--- fragments/src/main/java/com/github/pashmentov96/fragments/ListPersonsFragment.java
+++ fragments/src/main/java/com/github/pashmentov96/fragments/ListPersonsFragment.java
@@ -1,14 +1,19 @@
 package com.github.pashmentov96.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class ListPersonsFragment extends Fragment {
 
@@ -23,7 +28,7 @@ public class ListPersonsFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         RecyclerView recyclerView = view.findViewById(R.id.personRecyclerView);
@@ -34,7 +39,30 @@ public class ListPersonsFragment extends Fragment {
         final PersonAdapter adapter = new PersonAdapter();
         recyclerView.setAdapter(adapter);
 
-        StorageOfPersons.generate();
-        adapter.setPersonList((ViewHolderListener) getActivity(), StorageOfPersons.getPersonList());
+
+
+        class MyAsyncTask extends AsyncTask<Void, Integer, List<Person> > {
+            @Override
+            protected List<Person> doInBackground(Void... voids) {
+                Log.d("MyLogs", "begin");
+                StorageOfPersons.generate();
+                return StorageOfPersons.getPersonList();
+            }
+
+            @Override
+            protected void onPostExecute(List<Person> people) {
+                super.onPostExecute(people);
+                adapter.setPersonList((ViewHolderListener) getActivity(), people);
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                Toast.makeText(view.getContext(), String.valueOf(values[0]), Toast.LENGTH_LONG);
+            }
+        };
+
+        MyAsyncTask generatePersonsTask = new MyAsyncTask();
+        generatePersonsTask.execute();
     }
 }
