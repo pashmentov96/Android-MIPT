037b293d3739e76075029c41741613cf974161f0
nikita <nik.pashmentov@ya.ru>
2018-11-21 11:16:43 +0300

    Homework 8. Убрал контекст из переменных класса, добавил функцию для каждого кейса

--- app/src/main/java/com/github/pashmentov96/fragments/BackupService.java
+++ app/src/main/java/com/github/pashmentov96/fragments/BackupService.java
@@ -18,11 +18,9 @@ public class BackupService extends Service {
     private static final String READ_BACKUP = "ReadBackup";
     private static final String GET_ALL_BACKUPS = "GetAllBackups";
     private static final String FILENAME = "Filename";
-    private static Context context;
 
     public static void addBackup(Context context) {
         Log.d(LOG, "addBackup");
-        BackupService.context = context;
         Intent intent = new Intent(context, BackupService.class);
         intent.setAction(ADD_BACKUP);
         context.startService(intent);
@@ -30,7 +28,6 @@ public class BackupService extends Service {
 
     public static void readBackup(Context context, String filename) {
         Log.d(LOG, "readBackup");
-        BackupService.context = context;
         Intent intent = new Intent(context, BackupService.class);
         intent.setAction(READ_BACKUP);
         intent.putExtra(FILENAME, filename);
@@ -39,10 +36,48 @@ public class BackupService extends Service {
 
     public static void getAllBackups(Context context) {
         Log.d(LOG, "getAllBackups");
-        BackupService.context = context;
         Intent intent = new Intent(context, BackupService.class);
         intent.setAction(GET_ALL_BACKUPS);
         context.startService(intent);
+    }
+
+    private void addBackup() {
+        String filename = "backup-" + String.valueOf(Calendar.getInstance().getTime().getTime());
+        Log.d(LOG, filename);
+
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            PersonRepository personRepository = new PersonRepository(this);
+            List<Person> personList = personRepository.loadPersons();
+            outputStream.write(personList.toString().getBytes());
+            Log.d(LOG, personList.toString());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readBackup(String backupFilename) {
+        FileInputStream input;
+        try {
+            input = openFileInput(backupFilename);
+            byte[] bytes = new byte[input.available()];
+            int result = input.read(bytes);
+            Log.d(LOG, "!!! + " + result + " = " + new String(bytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getAllBackups() {
+        String[] files = this.fileList();
+        StringBuilder filesToString = new StringBuilder();
+        for (int i = 0; i < files.length; ++i) {
+            filesToString.append(files[i]);
+            filesToString.append("\n");
+        }
+        Log.d(LOG, "all files\n" + filesToString.toString());
     }
 
     @Override
@@ -51,41 +86,14 @@ public class BackupService extends Service {
 
         switch (intent.getAction()) {
             case ADD_BACKUP:
-                String filename = "backup-" + String.valueOf(Calendar.getInstance().getTime().getTime());
-                Log.d(LOG, filename);
-
-                FileOutputStream outputStream;
-                try {
-                    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-                    PersonRepository personRepository = new PersonRepository(context);
-                    List<Person> personList = personRepository.loadPersons();
-                    outputStream.write(personList.toString().getBytes());
-                    Log.d(LOG, personList.toString());
-                    outputStream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                addBackup();
                 break;
             case READ_BACKUP:
                 String backupFilename = intent.getStringExtra("FILENAME");
-                FileInputStream input;
-                try {
-                    input = openFileInput(backupFilename);
-                    byte[] bytes = new byte[input.available()];
-                    int result = input.read(bytes);
-                    Log.d(LOG, "!!! + " + result + " = " + new String(bytes));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                readBackup(backupFilename);
                 break;
             case GET_ALL_BACKUPS:
-                String[] files = context.fileList();
-                StringBuilder filesToString = new StringBuilder();
-                for (int i = 0; i < files.length; ++i) {
-                    filesToString.append(files[i]);
-                    filesToString.append("\n");
-                }
-                Log.d(LOG, "all files\n" + filesToString.toString());
+                getAllBackups();
                 break;
         }
         return super.onStartCommand(intent, flags, startId);
